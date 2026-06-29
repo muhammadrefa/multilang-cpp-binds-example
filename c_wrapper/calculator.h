98b3972587337d81cceb5e5592254266b766893d
Muhammad Refa Utama Putra <m.r3f4.u.p@gmail.com>
2026-06-29 22:37:57 +0700

    add defines and flags for windows

--- c_wrapper/calculator.h
+++ c_wrapper/calculator.h
@@ -1,4 +1,6 @@
 #pragma once
+
+#include "mylib_export.h"
 #include "status_code.h"
 
 #ifdef __cplusplus
@@ -8,12 +10,12 @@ extern "C"
 
 typedef struct mylib_calculator mylib_calculator_t;
 
-mylib_calculator_t* MyLib_Calculator_Create();
-mylib_status_t MyLib_Calculator_Destroy(mylib_calculator_t* handle);
-mylib_status_t MyLib_Calculator_Add(mylib_calculator_t* handle, int a, int b, int* retval);
-mylib_status_t MyLib_Calculator_Sub(mylib_calculator_t* handle, int a, int b, int* retval);
-mylib_status_t MyLib_Calculator_CntOpsDone(mylib_calculator_t* handle, int* retval);
-mylib_status_t MyLib_Calculator_ResetCounter(mylib_calculator_t* handle, int* retval);
+MYLIB_API mylib_calculator_t* MyLib_Calculator_Create();
+MYLIB_API mylib_status_t MyLib_Calculator_Destroy(mylib_calculator_t* handle);
+MYLIB_API mylib_status_t MyLib_Calculator_Add(mylib_calculator_t* handle, int a, int b, int* retval);
+MYLIB_API mylib_status_t MyLib_Calculator_Sub(mylib_calculator_t* handle, int a, int b, int* retval);
+MYLIB_API mylib_status_t MyLib_Calculator_CntOpsDone(mylib_calculator_t* handle, int* retval);
+MYLIB_API mylib_status_t MyLib_Calculator_ResetCounter(mylib_calculator_t* handle, int* retval);
 
 #ifdef __cplusplus
 }
